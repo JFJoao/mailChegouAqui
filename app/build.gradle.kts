da6f19685fad0e8d1a738408e93e254ada9cb17b
JFJoao <jfjoaojf@hotmail.com>
2024-06-11 20:49:55 -0300

    Atualização do Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,31 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Dependências do Google Play Services para autenticação OAuth 2.0
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+
+    // Dependências para a API do Gmail
+    implementation("com.google.api-client:google-api-client:1.32.2")
+    implementation("com.google.api-client:google-api-client-android:1.32.2")
+    implementation("com.google.apis:google-api-services-gmail:v1-rev20220504-1.32.1")
+
+    // Dependências para manipulação de JSON
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.13.3")
+
+    // Dependências para suporte ao Kotlin coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    // Dependências para injeção de dependências
+    implementation("org.koin:koin-android:3.3.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+
+
 }
