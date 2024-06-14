a9ef546f1f7aab445d244ffd962dee603fe26980
Pedro Klimaitis <pedro.klimaitis@gmail.com>
2024-06-14 12:15:38 -0300

    Criação do login com navController

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,14 @@ dependencies {
 
     // Dependências para injeção de dependências (opcional, se necessário)
     implementation("io.insert-koin:koin-android:3.3.0")
+    implementation("androidx.compose.foundation:foundation-layout-android:1.6.8")
+    implementation("androidx.compose.material3:material3-android:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling-preview-android:1.6.8")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    //Dependência do Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
